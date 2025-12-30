89aa740ce72e6979752e2aa23f1cb986e606be79
Otto Zhao <201915020123@sust.edu.cn>
2025-12-30 10:16:02 +0800

    feat: configure uvicorn to listen on all network interfaces by changing host to 0.0.0.0.

--- main.py
+++ main.py
@@ -131,4 +131,4 @@ async def predict(
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
